fc6fb385584863fd46d850ebdac1d31b377f77ac
Oliver <olichase91@gmail.com>
2023-03-20 09:05:20 -0700

    Sovled Lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    const questionTimer = setInterval(() => {
+     setTimeRemaining(timeRemaining - 1)
+    }, 1000)
+    if(timeRemaining === 0){
+      onAnswered(false)
+      setTimeRemaining(10)
+    }
+    return function cleanup() {
+      clearInterval(questionTimer)
+    }
+  })
+
+
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
